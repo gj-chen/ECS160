94da4f5ea1ef5a744973fb94b62d5d9c20acc67f
Gloria Chen <gjchen@ucdavis.edu>
2015-06-06 15:04:20 -0700

    login works ^_^

--- ParcelDeliveryService/app/src/main/java/cindy/myfirstapp/MyActivity.java
+++ ParcelDeliveryService/app/src/main/java/cindy/myfirstapp/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
@@ -18,6 +19,7 @@ import android.content.Intent;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -85,14 +87,102 @@ public class MyActivity extends ActionBarActivity {
     }
     /* called when the user clicks the Enter button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.username);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        new FetchSQL().execute();
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.username);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
     }
 
-    
+    //After the user presses enter / login, we have to check if user is in database
+    //if user is in database -> intent for friends page
+    //else send to registration page
+
+
+    private class FetchSQL extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params) {
+            String retval = "";
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                retval = e.toString();
+            }
+            String url = "jdbc:postgresql://10.0.2.2/postgres?user=postgres&password=05258729";
+            Connection connection = null;
+
+            //PreparedStatement statement_check = null;
+
+            EditText user = (EditText)findViewById(R.id.username);
+            String username = user.getText().toString();
+            EditText pass = (EditText)findViewById(R.id.password);
+            String password = pass.getText().toString();
+
+            //SQL commands
+            //String check = "SELECT * FROM users WHERE username = ? AND password = ?";
+            //String check = "SELECT * FROM users WHERE '"+username+"' = ? AND '"+password+"' = ?";
+            String check = "SELECT * FROM users WHERE username = '"+username+"' AND password = '"+password+"' ";
+            //String check = "SELECT username AND password FROM users WHERE username = ? AND password = ?";
+
+            try {
+                DriverManager.setLoginTimeout(5);
+                connection = DriverManager.getConnection(url);
+
+                //statement_check = connection.prepareStatement(check);
+                Statement statement_check = connection.createStatement();
+                //statement_check.setString(1, username);
+                //statement_check.setString(2, password);
+
+                ResultSet rs = statement_check.executeQuery(check);
+
+                //String u = rs.getString(1);
+                //String p = rs.getString(2);
+
+                //check database for login info
+                //if selection / login = TRUE, send to knapsack page
+                // if selection / login = FALSE, open registration page
+
+                if(rs.next()){
+                    System.out.println("inside check");
+                    //open toast: already registered user -> goes to login page
+                    //Toast.makeText(getApplicationContext(),
+                    //      "Registered User Already Exists", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), DisplayMessageActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    //Toast.makeText(getApplicationContext(),
+                    //      "Registration Successful! Please log in :)", Toast.LENGTH_LONG).show();
+                    System.out.println("inside else!!! wooooo!");
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), RegisterPage.class);
+                    startActivity(intent);
+                }
+
+                statement_check.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                retval = e.toString();
+                System.out.println(retval);
+            }
+            return retval;
+        }
+        @Override
+        protected void onPostExecute(String value) {
+            /*Toast.makeText(getApplicationContext(),
+                    "Registration Successful! Please log in :)", Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent();
+            intent.setClass(getApplicationContext(),MyActivity.class);
+            startActivity(intent);*/
+        }
+    }
+
+
 
     public void openSettings() {
         //
